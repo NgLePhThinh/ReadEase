f0729995c6956b9237194dce62d86f25c1b89ed1
ThinhNguyen <thinhbo2999@gmail.com>
2023-10-09 11:39:24 +0700

    clear unused import

--- src/main/java/com/ReadEase/ReadEase/Controller/User/AuthController.java
+++ src/main/java/com/ReadEase/ReadEase/Controller/User/AuthController.java
@@ -18,7 +18,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
